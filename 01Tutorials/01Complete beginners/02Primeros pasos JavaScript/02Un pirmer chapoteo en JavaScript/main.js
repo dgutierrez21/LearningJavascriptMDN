f7832016070109a0be94d7415bf1e15d26563f4f
Dixon G <dxxn0101@gmail.com>
2022-09-19 17:34:33 -0600

    JS | Ejemplo — Adivina el juego de números

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/02Un pirmer chapoteo en JavaScript/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/02Un pirmer chapoteo en JavaScript/main.js
@@ -13,3 +13,45 @@
 // Esto requiere una mezcla de trabajo duro, experiencia con la sintaxis de programación y práctica, además de un poco de creatividad. Cuanto más codifiques, mejor lo conseguirás. No podemos prometer que desarrollarás el "cerebro del programador" en cinco minutos, pero te daremos muchas oportunidades para practicar el pensamiento como un programador durante todo el curso.
 
 // Con eso en mente, veamos el ejemplo que construiremos en este artículo y revisemos el proceso general de diseccionarlo en tareas tangibles.
+
+// Ejemplo — Adivina el juego de números #008000
+// En este artículo te mostraremos cómo construir el juego simple que puedes ver a continuación:
+
+// ir a el siguiente enlace para ver el juego https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/A_first_splash#example_%E2%80%94_guess_the_number_game
+
+// Pruebe a jugarlo: familiarícese con el juego antes de seguir adelante.
+
+// Imaginemos que tu jefe te ha dado el siguiente resumen para crear este juego:
+
+// Quiero que crees un juego simple de adivinar el tipo de número. Debe elegir un número aleatorio entre 1 y 100, luego desafiar al jugador a adivinar el número en 10 turnos. Después de cada turno, se le debe decir al jugador si está bien o mal, y si está equivocado, si la suposición fue demasiado baja o demasiado alta. También debe decirle al jugador qué números adivinó previamente. El juego terminará una vez que el jugador adivine correctamente, o una vez que se quede sin turnos. Cuando el juego termina, el jugador debe tener la opción de comenzar a jugar de nuevo.
+
+// Al ver este resumen, lo primero que podemos hacer es comenzar a dividirlo en tareas simples y procesables, en la mayor medida posible de una mentalidad de programador:
+
+// 1. Genera un número aleatorio entre 1 y 100.
+
+// 2. Registre el número de turno en el que está el reproductor. Comience el 1.
+
+// 3. Proporcione al jugador una forma de adivinar cuál es el número.
+
+// 4. Una vez que se haya enviado una suposición, regístrela primero en algún lugar para que el usuario pueda ver sus conjeturas anteriores.
+
+// 5. A continuación, verifique si es el número correcto.
+
+// 6. Si es correcto:
+// 6.1 Mostrar mensaje de felicitación.
+// 6.2 Evitar que el jugador pueda ingresar más conjeturas (esto arruinaría el juego).
+// 6.3 Control de pantalla que permite al jugador reiniciar el juego.
+
+// 7. Si está mal y el jugador ha girado a la izquierda:
+// 7.1 Dígale al jugador que está equivocado y si su suposición fue demasiado alta o demasiado baja.
+// 7.2 Permítales ingresar otra suposición.
+// 7.3 Incremente el número de turno en 1.
+
+// 8. Si está mal y el jugador no tiene turnos a la izquierda:
+// 8.1 Dígale al jugador que ha terminado el juego.
+// 8.2 Evitar que el jugador pueda ingresar más conjeturas (esto arruinaría el juego).
+// 9.3 Control de pantalla que permite al jugador reiniciar el juego.
+
+// 9. Una vez que el juego se reinicie, asegúrate de que la lógica del juego y la interfaz de usuario estén completamente restablecidas, luego vuelve al paso 1.
+
+// Avancemos ahora, viendo cómo podemos convertir estos pasos en código, construyendo el ejemplo y explorando las características de JavaScript a medida que avanzamos.
