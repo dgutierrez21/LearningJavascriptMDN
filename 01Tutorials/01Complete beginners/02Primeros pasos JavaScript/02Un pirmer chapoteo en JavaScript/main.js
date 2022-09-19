efdd7ab98ab292f1e233e09d2f2a853b91b51887
Dixon G <dxxn0101@gmail.com>
2022-09-19 16:56:25 -0600

    JS | Pensar como un programador

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/02Un pirmer chapoteo en JavaScript/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/02Un pirmer chapoteo en JavaScript/main.js
@@ -5,3 +5,11 @@
 // Queremos establecer expectativas muy claras aquí: no se espera que aprenda JavaScript al final de este artículo, ni siquiera entienda todo el código que le pedimos que escriba. En su lugar, queremos darle una idea de cómo funcionan juntas las características de JavaScript y cómo se siente escribir JavaScript. En artículos posteriores, volverá a visitar todas las características que se muestran aquí con mucho más detalle, ¡así que no se preocupe si no lo entiende todo de inmediato!
 
 // Nota: Muchas de las características de código que verá en JavaScript son las mismas que en otros lenguajes de programación: funciones, bucles, etc. La sintaxis del código se ve diferente, pero los conceptos siguen siendo en gran medida los mismos.
+
+// Pensar como un programador #008000
+
+// Una de las cosas más difíciles de aprender en programación no es la sintaxis que necesita aprender, sino cómo aplicarla para resolver problemas del mundo real. Debe comenzar a pensar como un programador: esto generalmente implica mirar las descripciones de lo que su programa necesita hacer, determinar qué características de código se necesitan para lograr esas cosas y cómo hacer que funcionen juntas.
+
+// Esto requiere una mezcla de trabajo duro, experiencia con la sintaxis de programación y práctica, además de un poco de creatividad. Cuanto más codifiques, mejor lo conseguirás. No podemos prometer que desarrollarás el "cerebro del programador" en cinco minutos, pero te daremos muchas oportunidades para practicar el pensamiento como un programador durante todo el curso.
+
+// Con eso en mente, veamos el ejemplo que construiremos en este artículo y revisemos el proceso general de diseccionarlo en tareas tangibles.
