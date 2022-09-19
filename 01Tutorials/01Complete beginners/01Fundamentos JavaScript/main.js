04a8c60e34eb4aaf795fce7dfbe293f1a5d12d51
Dixon G <dxxn0101@gmail.com>
2022-09-18 18:34:45 -0600

    JS | ¿Qué es JavaScript?

--- 01Tutorials/01Complete beginners/01Fundamentos JavaScript/main.js
+++ 01Tutorials/01Complete beginners/01Fundamentos JavaScript/main.js
@@ -1,2 +1,19 @@
 // Conceptos básicos de JavaScript #ff0
 // JavaScript es un lenguaje de programación que agrega interactividad a su sitio web. Esto sucede en los juegos, en el comportamiento de las respuestas cuando se presionan botones o con la entrada de datos en los formularios; con estilo dinámico; con animación, etc. Este artículo le ayuda a comenzar con JavaScript y mejora su comprensión de lo que es posible.
+
+// ¿Qué es JavaScript? #008000
+// JavaScript es un poderoso lenguaje de programación que puede agregar interactividad a un sitio web. Fue inventado por Brendan Eich.
+
+// JavaScript es versátil y amigable para principiantes. Con más experiencia, podrás crear juegos, gráficos animados en 2D y 3D, aplicaciones completas basadas en bases de datos y mucho más.
+
+// JavaScript en sí es relativamente compacto, pero muy flexible. Los desarrolladores han escrito una variedad de herramientas sobre el lenguaje JavaScript central, desbloqueando una gran cantidad de funcionalidad con el mínimo esfuerzo. Estos incluyen:
+
+// Interfaces de programación de aplicaciones (API) del navegador integradas en los navegadores web, que proporcionan funcionalidades como la creación dinámica de HTML y la configuración de estilos CSS; recopilar y manipular una secuencia de vídeo desde la cámara web de un usuario, o generar gráficos 3D y muestras de audio.
+
+// API de terceros que permiten a los desarrolladores incorporar funcionalidad en sitios de otros proveedores de contenido, como Twitter o Facebook.
+
+// Marcos y bibliotecas de terceros que puede aplicar a HTML para acelerar el trabajo de creación de sitios y aplicaciones.
+
+// Está fuera del alcance de este artículo, como una ligera introducción a JavaScript, presentar los detalles de cómo el lenguaje JavaScript central es diferente de las herramientas enumeradas anteriormente. Puede obtener más información en el área de aprendizaje de JavaScript de MDN, así como en otras partes de MDN.
+
+// La siguiente sección presenta algunos aspectos del lenguaje principal y ofrece la oportunidad de jugar con algunas características de la API del navegador también. ¡Que te diviertas!
