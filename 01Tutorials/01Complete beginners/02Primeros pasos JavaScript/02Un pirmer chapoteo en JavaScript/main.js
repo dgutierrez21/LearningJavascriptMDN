b596199ea6188afdd8cafa83ca5234d9dfda5dfe
Dixon G <dxxn0101@gmail.com>
2022-09-19 16:54:44 -0600

    JS | Un primer chapuzón en JavaScript

---
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/02Un pirmer chapoteo en JavaScript/main.js
@@ -0,0 +1,7 @@
+// Un primer chapuzón en JavaScript #ff0
+
+// Ahora que has aprendido algo sobre la teoría de JavaScript y lo que puedes hacer con él, te vamos a dar una idea de cómo es el proceso de creación de un programa JavaScript simple, guiándote a través de un tutorial práctico. Aquí construirás un simple juego de "Adivina el número", paso a paso.
+
+// Queremos establecer expectativas muy claras aquí: no se espera que aprenda JavaScript al final de este artículo, ni siquiera entienda todo el código que le pedimos que escriba. En su lugar, queremos darle una idea de cómo funcionan juntas las características de JavaScript y cómo se siente escribir JavaScript. En artículos posteriores, volverá a visitar todas las características que se muestran aquí con mucho más detalle, ¡así que no se preocupe si no lo entiende todo de inmediato!
+
+// Nota: Muchas de las características de código que verá en JavaScript son las mismas que en otros lenguajes de programación: funciones, bucles, etc. La sintaxis del código se ve diferente, pero los conceptos siguen siendo en gran medida los mismos.
