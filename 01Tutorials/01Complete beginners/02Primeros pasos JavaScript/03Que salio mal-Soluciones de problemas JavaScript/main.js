10b4c552dcfe8f967598fcb7becee194c409804d
Dixon G <dxxn0101@gmail.com>
2022-09-20 13:21:17 -0600

    JS | Tipos de error

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/03Que salio mal-Soluciones de problemas JavaScript/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/03Que salio mal-Soluciones de problemas JavaScript/main.js
@@ -1,2 +1,11 @@
 // ¿Qué salió mal? Solución de problemas de JavaScript #008000
 // Cuando construiste el juego "Adivina el número" en el artículo anterior, es posible que hayas descubierto que no funcionó. Nunca temas: este artículo tiene como objetivo salvarte de arrancarte el pelo por tales problemas al proporcionarte algunos consejos sobre cómo encontrar y corregir errores en los programas JavaScript.
+
+// Tipos de error #008000
+// En términos generales, cuando haces algo mal en el código, hay dos tipos principales de errores que encontrarás:
+
+// Errores de sintaxis: Estos son errores ortográficos en su código que en realidad hacen que el programa no se ejecute en absoluto, o deje de funcionar a mitad de camino; por lo general, también se le proporcionarán algunos mensajes de error. Por lo general, estos están bien para solucionar, siempre y cuando esté familiarizado con las herramientas adecuadas y sepa lo que significan los mensajes de error.
+
+// Errores lógicos: Estos son errores en los que la sintaxis es realmente correcta pero el código no es lo que pretendía que fuera, lo que significa que el programa se ejecuta correctamente pero da resultados incorrectos. A menudo son más difíciles de corregir que los errores de sintaxis, ya que generalmente no hay un mensaje de error que lo dirija a la fuente del error.
+
+// De acuerdo, así que no es tan simple: hay algunos otros diferenciadores a medida que profundizas. Pero las clasificaciones anteriores funcionarán en esta etapa temprana de su carrera. Veremos ambos tipos en el futuro.
