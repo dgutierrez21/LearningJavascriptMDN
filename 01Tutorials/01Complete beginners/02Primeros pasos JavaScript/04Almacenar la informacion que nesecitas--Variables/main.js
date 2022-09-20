979f597142b946786caf23c13e2b7ebc1478c3da
Dixon G <dxxn0101@gmail.com>
2022-09-20 17:55:18 -0600

    JS | Herramientas que necesitas

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/04Almacenar la informacion que nesecitas--Variables/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/04Almacenar la informacion que nesecitas--Variables/main.js
@@ -1,2 +1,5 @@
 // Almacenamiento de la información que necesita — Variables #ff0
 // Después de leer el último par de artículos, ahora debe saber qué es JavaScript, qué puede hacer por usted, cómo lo usa junto con otras tecnologías web y cómo se ven sus características principales desde un alto nivel. En este artículo, llegaremos a los conceptos básicos reales, analizando cómo trabajar con los bloques de construcción más básicos de JavaScript: variables.
+
+// Herramientas que necesitas #008000
+// A lo largo de este artículo, se le pedirá que escriba líneas de código para probar su comprensión del contenido. Si está utilizando un navegador de escritorio, el mejor lugar para escribir el código de ejemplo es la consola JavaScript de su navegador (consulte ¿Qué son las herramientas de desarrollo del navegador para obtener más información sobre cómo acceder a esta herramienta?).
