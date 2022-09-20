053e9d0c68ab2492e8eb343e3b9763eb91093d98
Dixon G <dxxn0101@gmail.com>
2022-09-20 13:25:36 -0600

    JS | un ejemplo erróneo

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/03Que salio mal-Soluciones de problemas JavaScript/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/03Que salio mal-Soluciones de problemas JavaScript/main.js
@@ -9,3 +9,14 @@
 // Errores lógicos: Estos son errores en los que la sintaxis es realmente correcta pero el código no es lo que pretendía que fuera, lo que significa que el programa se ejecuta correctamente pero da resultados incorrectos. A menudo son más difíciles de corregir que los errores de sintaxis, ya que generalmente no hay un mensaje de error que lo dirija a la fuente del error.
 
 // De acuerdo, así que no es tan simple: hay algunos otros diferenciadores a medida que profundizas. Pero las clasificaciones anteriores funcionarán en esta etapa temprana de su carrera. Veremos ambos tipos en el futuro.
+
+// Un ejemplo erróneo #008000
+// Para comenzar, volvamos a nuestro juego de adivinanzas de números, excepto que esta vez exploraremos una versión que tiene algunos errores deliberados introducidos. Vaya a GitHub y hágase una copia local de los errores de juego de números.html (véalo en vivo aquí).
+
+// Para comenzar, abra la copia local dentro de su editor de texto favorito y su navegador.
+
+// Intenta jugar el juego: notarás que cuando presionas el botón "Enviar conjetura", ¡no funciona!
+
+// Nota: Es posible que tenga su propia versión del ejemplo del juego que no funciona, ¡que es posible que desee solucionar! Todavía nos gustaría que trabajaras en el artículo con nuestra versión, para que puedas aprender las técnicas que estamos enseñando aquí. Luego puedes volver atrás e intentar arreglar tu ejemplo.
+
+// En este punto, consultemos la consola del desarrollador para ver si informa algún error de sintaxis, luego intente solucionarlos. Aprenderá cómo a continuación.
