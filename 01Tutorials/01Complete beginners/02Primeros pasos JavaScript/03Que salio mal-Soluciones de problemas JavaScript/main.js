1d47a06ae8dce09eb13bb0825df82ad77e357cad
Dixon G <dxxn0101@gmail.com>
2022-09-20 13:07:55 -0600

    JS | ¿Qué salió mal? Solución de problemas de JavaScript

---
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/03Que salio mal-Soluciones de problemas JavaScript/main.js
@@ -0,0 +1,2 @@
+// ¿Qué salió mal? Solución de problemas de JavaScript #008000
+// Cuando construiste el juego "Adivina el número" en el artículo anterior, es posible que hayas descubierto que no funcionó. Nunca temas: este artículo tiene como objetivo salvarte de arrancarte el pelo por tales problemas al proporcionarte algunos consejos sobre cómo encontrar y corregir errores en los programas JavaScript.
