6095c600d80a3f7bf8c36c1407d593fa69612b4d
Dixon G <dxxn0101@gmail.com>
2022-09-20 17:54:43 -0600

    JS | Almacenamiento de la información que necesita — Variables

---
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/04Almacenar la informacion que nesecitas--Variables/main.js
@@ -0,0 +1,2 @@
+// Almacenamiento de la información que necesita — Variables #ff0
+// Después de leer el último par de artículos, ahora debe saber qué es JavaScript, qué puede hacer por usted, cómo lo usa junto con otras tecnologías web y cómo se ven sus características principales desde un alto nivel. En este artículo, llegaremos a los conceptos básicos reales, analizando cómo trabajar con los bloques de construcción más básicos de JavaScript: variables.
