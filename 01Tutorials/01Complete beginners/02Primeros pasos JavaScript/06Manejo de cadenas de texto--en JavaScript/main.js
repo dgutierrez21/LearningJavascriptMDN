398bc666b30bc50027533632fb640acdf6496acd
Dixon G <dxxn0101@gmail.com>
2022-09-21 13:32:10 -0600

    JS | El poder de las palabras

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/06Manejo de cadenas de texto--en JavaScript/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/06Manejo de cadenas de texto--en JavaScript/main.js
@@ -2,3 +2,7 @@
 
 // A continuación, dirigiremos nuestra atención a las cadenas: así es como se llaman las piezas de texto en la programación. En este artículo, veremos todas las cosas comunes que realmente debe saber sobre las cadenas al aprender JavaScript, como crear cadenas, escapar de las comillas en las cadenas y unir las cadenas.
 
+// El poder de las palabras #008000
+// Las palabras son muy importantes para los humanos, son una gran parte de cómo nos comunicamos. Dado que la Web es un medio en gran parte basado en texto diseñado para permitir que los humanos se comuniquen y compartan información, es útil para nosotros tener control sobre las palabras que aparecen en ella. HTML proporciona estructura y significado a nuestro texto, CSS nos permite diseñarlo con precisión, y JavaScript contiene una serie de características para manipular cadenas, crear mensajes de bienvenida personalizados e indicaciones, mostrar las etiquetas de texto correctas cuando sea necesario, ordenar los términos en el orden deseado y mucho más.
+
+// Casi todos los programas que le hemos mostrado hasta ahora en el curso han involucrado alguna manipulación de cadenas.
