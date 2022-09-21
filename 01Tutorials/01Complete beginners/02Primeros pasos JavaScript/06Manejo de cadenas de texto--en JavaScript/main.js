604938b7dc8421c833ba4dfb31fc48deb245fb87
Dixon G <dxxn0101@gmail.com>
2022-09-21 13:30:32 -0600

    JS | Manejo de texto: cadenas en JavaScript

---
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/06Manejo de cadenas de texto--en JavaScript/main.js
@@ -0,0 +1,4 @@
+// Manejo de texto: cadenas en JavaScript #008000
+
+// A continuación, dirigiremos nuestra atención a las cadenas: así es como se llaman las piezas de texto en la programación. En este artículo, veremos todas las cosas comunes que realmente debe saber sobre las cadenas al aprender JavaScript, como crear cadenas, escapar de las comillas en las cadenas y unir las cadenas.
+
