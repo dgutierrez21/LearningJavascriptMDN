8cdfcdae20b2ce3fa9152df3c3d7aaf4d0b285cc
Dixon G <dxxn0101@gmail.com>
2022-09-22 17:47:11 -0600

    JS | Matrices

---
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/08Arrays/main.js
@@ -0,0 +1,2 @@
+// Matrices #ff0
+// En el artículo final de este módulo, veremos las matrices, una forma ordenada de almacenar una lista de elementos de datos bajo un solo nombre de variable. Aquí veremos por qué esto es útil, luego exploramos cómo crear una matriz, recuperar, agregar y eliminar elementos almacenados en una matriz, y más.
