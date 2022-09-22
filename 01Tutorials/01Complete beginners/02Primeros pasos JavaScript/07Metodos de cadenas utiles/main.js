5e8d01d392294aaf7443ae675598f9bc62f3615b
Dixon G <dxxn0101@gmail.com>
2022-09-21 18:31:04 -0600

    JS | Cadenas como objetos

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/07Metodos de cadenas utiles/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/07Metodos de cadenas utiles/main.js
@@ -1,3 +1,13 @@
 // Métodos de cadena útiles #ff0
 // Ahora que hemos analizado los conceptos básicos de las cadenas, subamos una marcha y comencemos a pensar en qué operaciones útiles podemos hacer en las cadenas con métodos incorporados, como encontrar la longitud de una cadena de texto, unir y dividir cadenas, sustituir un carácter en una cadena por otro y más.
 
+// Cadenas como objetos #008000
+// La mayoría de las cosas son objetos en JavaScript. Al crear una cadena, por ejemplo, mediante el uso de
+
+const string = "Esta es mi cadena";
+
+// la variable se convierte en una instancia de objeto de cadena y, como resultado, tiene un gran número de propiedades y métodos disponibles. ¡Puede ver esto si va a la página del objeto String y mira hacia abajo en la lista en el costado de la página!
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+// Ahora, antes de que tu cerebro comience a derretirse, ¡no te preocupes! Realmente no necesita saber sobre la mayoría de estos al principio de su viaje de aprendizaje. Pero hay algunos que potencialmente usará con bastante frecuencia que veremos aquí.
