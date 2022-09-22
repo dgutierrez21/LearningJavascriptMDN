43d5c16d870a2c009d5b6e34d08729c49fec3093
Dixon G <dxxn0101@gmail.com>
2022-09-21 18:20:47 -0600

    JS | Métodos de cadena útiles

---
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/07Metodos de cadenas utiles/main.js
@@ -0,0 +1,3 @@
+// Métodos de cadena útiles #ff0
+// Ahora que hemos analizado los conceptos básicos de las cadenas, subamos una marcha y comencemos a pensar en qué operaciones útiles podemos hacer en las cadenas con métodos incorporados, como encontrar la longitud de una cadena de texto, unir y dividir cadenas, sustituir un carácter en una cadena por otro y más.
+
