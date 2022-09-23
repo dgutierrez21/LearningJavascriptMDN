0a43d1208514a16573740c07c72c25c346f2b6f7
Dixon G <dxxn0101@gmail.com>
2022-09-22 18:07:46 -0600

    JS | ¿Qué es una matriz?

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/08Arrays/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/08Arrays/main.js
@@ -1,2 +1,7 @@
 // Matrices #ff0
 // En el artículo final de este módulo, veremos las matrices, una forma ordenada de almacenar una lista de elementos de datos bajo un solo nombre de variable. Aquí veremos por qué esto es útil, luego exploramos cómo crear una matriz, recuperar, agregar y eliminar elementos almacenados en una matriz, y más.
+
+// ¿Qué es una matriz? #008000
+// Las matrices generalmente se describen como "objetos similares a listas"; son básicamente objetos individuales que contienen múltiples valores almacenados en una lista. Los objetos de matriz se pueden almacenar en variables y tratar de la misma manera que cualquier otro tipo de valor, la diferencia es que podemos acceder a cada valor dentro de la lista individualmente, y hacer cosas súper útiles y eficientes con la lista, como recorrerla y hacer lo mismo con cada valor. Tal vez tenemos una serie de artículos de productos y sus precios almacenados en una matriz, y queremos recorrerlos todos e imprimirlos en una factura, mientras totalizamos todos los precios juntos e imprimimos el precio total en la parte inferior.
+
+// Si no tuviéramos matrices, tendríamos que almacenar cada elemento en una variable separada, luego llamar al código que hace la impresión y agregar por separado para cada elemento. Esto sería mucho más largo de escribir, menos eficiente y más propenso a errores. Si tuviéramos 10 artículos para agregar a la factura ya sería molesto, pero ¿qué pasa con 100 artículos, o 1000? Volveremos a este ejemplo más adelante en el artículo.
