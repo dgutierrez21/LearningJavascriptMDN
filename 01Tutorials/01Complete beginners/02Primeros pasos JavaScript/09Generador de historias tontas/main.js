d1a03de274a49d91b0c16c55a89ca4c1cf8cb61a
Dixon G <dxxn0101@gmail.com>
2022-09-24 12:27:55 -0600

    JS | Generador de historias tontas finalizado

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/09Generador de historias tontas/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/09Generador de historias tontas/main.js
@@ -85,3 +85,60 @@
 // Math.round() es un método JavaScript integrado que redondea el resultado de un cálculo al número entero más cercano.
 
 // Hay tres instancias de cadenas que deben reemplazarse. Puede repetir el método replace() varias veces o puede usar replaceAll(). Recuerda, ¡las cuerdas son inmutables!
+
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
+const customName = document.getElementById("customname");
+const randomize = document.querySelector(".randomize");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// 2. RAW TEXT STRINGS
+
+let storyText =
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"],
+  insertY = ["the soup kitchen", "Disneyland", "the White House"],
+  insertZ = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and crawled away",
+  ];
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener("click", result);
+
+function result() {
+  let newStory = storyText;
+
+  let xItem = randomValueFromArray(insertX),
+    yItem = randomValueFromArray(insertY),
+    zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(":insertx:", xItem);
+  newStory = newStory.replaceAll(":inserty:", yItem);
+  newStory = newStory.replaceAll(".insertz", zItem);
+
+  if (customName.value !== "") {
+    const name = customName.value;
+
+    newStory = newStory.replaceAll("Bob", name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = Math.round((300 * 1) / 14) + " Stone";
+    const temperature = Math.round((94 - 32) / 1.8) + " Centigrade";
+
+    newStory = newStory.replaceAll("94 fahrenheit", temperature);
+    newStory = newStory.replaceAll("300 pounds", weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
