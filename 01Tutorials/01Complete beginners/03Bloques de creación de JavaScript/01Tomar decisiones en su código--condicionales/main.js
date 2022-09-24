2ba2cd541c76741aec41c7edf192b4bef334c687
Dixon G <dxxn0101@gmail.com>
2022-09-24 13:31:37 -0600

    JS | ¡Puedes tenerlo con una condición!

--- 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/01Tomar decisiones en su código--condicionales/main.js
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/01Tomar decisiones en su código--condicionales/main.js
@@ -1,3 +1,9 @@
 // Tomar decisiones en el código: condicionales #008000
 
 // En cualquier lenguaje de programación, el código necesita tomar decisiones y llevar a cabo acciones en consecuencia dependiendo de las diferentes entradas. Por ejemplo, en un juego, si el número de vidas del jugador es 0, entonces se acabó el juego. En una aplicación meteorológica, si se está mirando por la mañana, muestre un gráfico del amanecer; mostrar estrellas y una luna si es de noche. En este artículo, exploraremos cómo funcionan las llamadas declaraciones condicionales en JavaScript.
+
+// ¡Puedes tenerlo con una condición! #008000
+// Los seres humanos (y otros animales) toman decisiones todo el tiempo que afectan sus vidas, desde pequeñas ("¿debo comer una galleta o dos?") hasta grandes ("¿debo quedarme en mi país de origen y trabajar en la granja de mi padre, o debo mudarme a Estados Unidos y estudiar astrofísica?")
+
+// Las declaraciones condicionales nos permiten representar dicha toma de decisiones en JavaScript, desde la elección que se debe hacer (por ejemplo, "una galleta o dos"), hasta el resultado resultante de esas elecciones (tal vez el resultado de "comió una galleta" podría ser "todavía sentía hambre", y el resultado de "comió dos galletas" podría ser "se sintió lleno, pero mamá me regañó por comer todas las galletas").
+
