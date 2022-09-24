bf3c06ec535815a29edb37369dfebabd350786af
Dixon G <dxxn0101@gmail.com>
2022-09-24 09:48:10 -0600

    JS | resumen de proyecto

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/09Generador de historias tontas/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/09Generador de historias tontas/main.js
@@ -12,3 +12,11 @@
 // Alternativamente, puede usar un sitio como JSBin o Glitch para hacer su evaluación. Puede pegar el HTML, CSS y JavaScript en uno de estos editores en línea. Si el editor en línea que está utilizando no tiene un panel JavaScript separado, no dude en ponerlo en línea en un elemento <script> dentro de la página HTML.
 
 // Nota: Si se queda atascado, pídanos ayuda: consulte la sección Evaluación o ayuda adicional en la parte inferior de esta página.
+
+// Resumen del proyecto #008000
+// Se le han proporcionado algunos HTML / CSS sin procesar y algunas cadenas de texto y funciones de JavaScript; debe escribir el JavaScript necesario para convertirlo en un programa de trabajo, que hace lo siguiente:
+
+// Genera una historia tonta cuando se presiona el botón "Generar historia aleatoria".
+// Reemplaza el nombre predeterminado "Bob" en la historia con un nombre personalizado, solo si se ingresa un nombre personalizado en el campo de texto "Ingresar nombre personalizado" antes de presionar el botón generar.
+// Convierte las cantidades y unidades predeterminadas de peso y temperatura de EE. UU. en la historia en equivalentes del Reino Unido si se marca el botón de opción del Reino Unido antes de presionar el botón de generar.
+// Genera una nueva historia tonta aleatoria cada vez que se presiona el botón.
