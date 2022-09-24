febe5240f7b444df6e4318afffce5a6f8a7ea9d2
Dixon G <dxxn0101@gmail.com>
2022-09-24 09:43:34 -0600

    JS | Generador de historias tontas

---
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/09Generador de historias tontas/main.js
@@ -0,0 +1,4 @@
+// Generador de historias tontas #008000
+
+// En esta evaluación, se le asignará la tarea de tomar parte del conocimiento que ha adquirido en los artículos de este módulo y aplicarlo a la creación de una aplicación divertida que genere historias tontas aleatorias. ¡Que te diviertas!
+
