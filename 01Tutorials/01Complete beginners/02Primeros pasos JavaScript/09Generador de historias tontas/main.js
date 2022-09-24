047d49478f02adf482dce2cb3a657cfa4f24400c
Dixon G <dxxn0101@gmail.com>
2022-09-24 09:45:42 -0600

    JS | punto de partida

--- 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/09Generador de historias tontas/main.js
+++ 01Tutorials/01Complete beginners/02Primeros pasos JavaScript/09Generador de historias tontas/main.js
@@ -2,3 +2,13 @@
 
 // En esta evaluación, se le asignará la tarea de tomar parte del conocimiento que ha adquirido en los artículos de este módulo y aplicarlo a la creación de una aplicación divertida que genere historias tontas aleatorias. ¡Que te diviertas!
 
+// punto de partida #008000
+// Para comenzar esta evaluación, usted debe:
+
+// Vaya y tome el archivo https://github.com/mdn/learning-area/blob/main/javascript/introduction-to-js-1/assessment-start/index.html para el ejemplo, guarde una copia local del mismo como index.html en un nuevo directorio en algún lugar de su computadora y haga la evaluación localmente para empezar. Esto también tiene el CSS para aplicar estilo al ejemplo contenido en él.
+
+// Vaya a la página que contiene el texto sin procesar https://github.com/mdn/learning-area/blob/main/javascript/introduction-to-js-1/assessment-start/raw-text.txt y manténgalo abierto en una pestaña separada del navegador en algún lugar. Lo necesitarás más tarde.
+
+// Alternativamente, puede usar un sitio como JSBin o Glitch para hacer su evaluación. Puede pegar el HTML, CSS y JavaScript en uno de estos editores en línea. Si el editor en línea que está utilizando no tiene un panel JavaScript separado, no dude en ponerlo en línea en un elemento <script> dentro de la página HTML.
+
+// Nota: Si se queda atascado, pídanos ayuda: consulte la sección Evaluación o ayuda adicional en la parte inferior de esta página.
