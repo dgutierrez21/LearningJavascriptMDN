3e50a10da6dc9ed25249eeebe7fae9c02919a6c7
Dixon G <dxxn0101@gmail.com>
2022-09-24 13:29:59 -0600

    JS | Tomar decisiones en el código: condicionales

---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/01Tomar decisiones en su código--condicionales/main.js
@@ -0,0 +1,3 @@
+// Tomar decisiones en el código: condicionales #008000
+
+// En cualquier lenguaje de programación, el código necesita tomar decisiones y llevar a cabo acciones en consecuencia dependiendo de las diferentes entradas. Por ejemplo, en un juego, si el número de vidas del jugador es 0, entonces se acabó el juego. En una aplicación meteorológica, si se está mirando por la mañana, muestre un gráfico del amanecer; mostrar estrellas y una luna si es de noche. En este artículo, exploraremos cómo funcionan las llamadas declaraciones condicionales en JavaScript.
