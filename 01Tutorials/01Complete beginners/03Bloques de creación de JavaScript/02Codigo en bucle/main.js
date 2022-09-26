0323f27894e2f9b19aaddd3c899a0be598ea4e8f
Dixon G <dxxn0101@gmail.com>
2022-09-25 18:09:17 -0600

    JS | Código en bucle

---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/02Codigo en bucle/main.js
@@ -0,0 +1,2 @@
+// Código en bucle #ff0
+// Los lenguajes de programación son muy útiles para completar rápidamente tareas repetitivas, desde múltiples cálculos básicos hasta casi cualquier otra situación en la que tenga muchos elementos de trabajo similares que completar. Aquí veremos las estructuras de bucle disponibles en JavaScript que manejan tales necesidades.
