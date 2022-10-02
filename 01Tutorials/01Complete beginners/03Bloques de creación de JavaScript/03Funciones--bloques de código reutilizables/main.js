b906d47938542a14be76cdbb9e35a6363a18f5f2
Dixon G <dxxn0101@gmail.com>
2022-10-02 17:42:47 -0600

    JS | Funciones: bloques de código reutilizables

---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/03Funciones--bloques de código reutilizables/main.js
@@ -0,0 +1,3 @@
+// Funciones: bloques de código reutilizables #ff0
+
+// Otro concepto esencial en la codificación son las funciones, que le permiten almacenar una pieza de código que realiza una sola tarea dentro de un bloque definido, y luego llamar a ese código cuando lo necesite utilizando un solo comando corto, en lugar de tener que escribir el mismo código varias veces. En este artículo exploraremos conceptos fundamentales detrás de funciones como la sintaxis básica, cómo invocarlas y definirlas, el alcance y los parámetros.
