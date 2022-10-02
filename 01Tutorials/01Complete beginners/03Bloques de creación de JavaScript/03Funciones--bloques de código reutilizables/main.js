9f03b11519bfbc2284ca9ddece7ff662eaedc37d
Dixon G <dxxn0101@gmail.com>
2022-10-02 17:45:41 -0600

    JS | ¿Dónde puedo encontrar funciones?

--- 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/03Funciones--bloques de código reutilizables/main.js
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/03Funciones--bloques de código reutilizables/main.js
@@ -1,3 +1,8 @@
 // Funciones: bloques de código reutilizables #ff0
 
 // Otro concepto esencial en la codificación son las funciones, que le permiten almacenar una pieza de código que realiza una sola tarea dentro de un bloque definido, y luego llamar a ese código cuando lo necesite utilizando un solo comando corto, en lugar de tener que escribir el mismo código varias veces. En este artículo exploraremos conceptos fundamentales detrás de funciones como la sintaxis básica, cómo invocarlas y definirlas, el alcance y los parámetros.
+
+// ¿Dónde puedo encontrar funciones? #008000
+// En JavaScript, encontrarás funciones en todas partes. De hecho, hemos estado usando funciones durante todo el curso hasta ahora; simplemente no hemos estado hablando mucho de ellos. Sin embargo, ahora es el momento de que comencemos a hablar de funciones explícitamente y realmente exploremos su sintaxis.
+
+// Prácticamente cada vez que hace uso de una estructura de JavaScript que presenta un par de paréntesis, () y no está utilizando una estructura de lenguaje incorporada común como un bucle for, while o do ... while bucle, o if... else declaración, usted está haciendo uso de una función.
