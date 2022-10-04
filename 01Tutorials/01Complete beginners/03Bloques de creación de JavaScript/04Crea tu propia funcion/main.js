7ca74b4d774a918e94d61244eae1f0e587f7af8c
Dixon G <dxxn0101@gmail.com>
2022-10-04 08:10:53 -0600

    JS | Crea tu propia función

---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/04Crea tu propia funcion/main.js
@@ -0,0 +1,3 @@
+// Crea tu propia función #ff0
+
+// Con la mayor parte de la teoría esencial tratada en el artículo anterior, este artículo proporciona experiencia práctica. Aquí obtendrá algo de práctica para construir su propia función personalizada. En el camino, también explicaremos algunos detalles útiles para tratar con las funciones.
