8ae683d27b9e6d4aec0d2e9027090588eac1cddc
Dixon G <dxxn0101@gmail.com>
2022-10-04 16:44:15 -0600

    JS | valores devueltos de la función

---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/05Valores devueltos de la funcion/main.js
@@ -0,0 +1,3 @@
+// Valores devueltos de la función #ff0
+
+// Hay un último concepto esencial sobre las funciones que debemos discutir: los valores de retorno. Algunas funciones no devuelven un valor significativo, pero otras sí. Es importante comprender cuáles son sus valores, cómo usarlos en el código y cómo hacer que las funciones devuelvan valores útiles. Cubriremos todo esto a continuación.
