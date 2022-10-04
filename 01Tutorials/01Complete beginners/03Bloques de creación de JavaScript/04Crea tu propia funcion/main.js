d21f8368ae7a3133a7d25e15385a342f31a64d91
Dixon G <dxxn0101@gmail.com>
2022-10-04 08:13:01 -0600

    JS |  Aprendizaje activo: construyamos una función

--- 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/04Crea tu propia funcion/main.js
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/04Crea tu propia funcion/main.js
@@ -1,3 +1,14 @@
 // Crea tu propia función #ff0
 
 // Con la mayor parte de la teoría esencial tratada en el artículo anterior, este artículo proporciona experiencia práctica. Aquí obtendrá algo de práctica para construir su propia función personalizada. En el camino, también explicaremos algunos detalles útiles para tratar con las funciones.
+
+// Aprendizaje activo: construyamos una función #008000
+// La función personalizada que vamos a construir se llamará displayMessage(). Mostrará un cuadro de mensaje personalizado en una página web y actuará como un reemplazo personalizado para la función alert() incorporada de un navegador. Hemos visto esto antes, pero vamos a refrescar nuestros recuerdos. Escriba lo siguiente en la consola JavaScript de su navegador, en cualquier página que desee:
+
+// alert("Este es un mensaje");
+
+// La función de alert toma un solo argumento: la cadena que se muestra en el cuadro de alerta. Intente variar la cadena para cambiar el mensaje.
+
+// La función alert es limitada: puede alterar el mensaje, pero no puede variar fácilmente nada más, como el color, el icono o cualquier otra cosa. Construiremos uno que resultará ser más divertido.
+
+// Nota: Este ejemplo debería funcionar bien en todos los navegadores modernos, pero el estilo puede parecer un poco divertido en navegadores un poco más antiguos. Te recomendamos que hagas este ejercicio en un navegador moderno como Firefox, Opera o Chrome.
