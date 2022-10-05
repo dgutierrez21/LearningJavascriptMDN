66218fded1052bf619a2164c6b96c453aca718c2
Dixon G <dxxn0101@gmail.com>
2022-10-04 21:10:13 -0600

    JS | Introducción a los eventos

---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/06Introducción a los eventos/main.js
@@ -0,0 +1,4 @@
+// Introducción a los eventos #ff0
+// Los eventos son acciones u ocurrencias que ocurren en el sistema que está programando, que el sistema le informa para que su código pueda reaccionar a ellos.
+
+// Por ejemplo, si el usuario hace clic en un botón de una página web, es posible que desee reaccionar a esa acción mostrando un cuadro de información. En este artículo, discutimos algunos conceptos importantes que rodean a los eventos y observamos cómo funcionan en los navegadores. Este no será un estudio exhaustivo; justo lo que necesitas saber en esta etapa.
