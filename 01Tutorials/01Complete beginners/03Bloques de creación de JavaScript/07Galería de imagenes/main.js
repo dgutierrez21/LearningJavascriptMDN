6bfcdab0303ad34344a48f1c3b08e9d48dc5697a
Dixon G <dxxn0101@gmail.com>
2022-10-08 09:39:03 -0600

    JS | Galería de imagenes

---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/07Galería de imagenes/main.js
@@ -0,0 +1,3 @@
+// Galería de imágenes #ff0
+
+// Ahora que hemos analizado los bloques de construcción fundamentales de JavaScript, pondremos a prueba su conocimiento de bucles, funciones, condicionales y eventos al hacer que cree un elemento bastante común que verá en muchos sitios web: una galería de imágenes con JavaScript.
