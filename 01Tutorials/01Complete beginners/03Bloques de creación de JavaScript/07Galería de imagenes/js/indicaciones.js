46f9059c4ea3bfef25cdfafaea158d8163f705c3
Dixon G <dxxn0101@gmail.com>
2022-10-08 09:48:48 -0600

    JS | punto

---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/07Galería de imagenes/js/indicaciones.js
@@ -0,0 +1,8 @@
+// Galería de imágenes #ff0
+
+// Ahora que hemos analizado los bloques de construcción fundamentales de JavaScript, pondremos a prueba su conocimiento de bucles, funciones, condicionales y eventos al hacer que cree un elemento bastante común que verá en muchos sitios web: una galería de imágenes con JavaScript.
+
+// Punto #008000
+// Para comenzar esta evaluación, debe ir y tomar el archivo ZIP para el ejemplo, descomprimirlo en algún lugar de su computadora y hacer el ejercicio localmente para empezar.
+
+// Alternativamente, puede usar un sitio como JSBin o Glitch para hacer su evaluación. Puede pegar el HTML, CSS y JavaScript en uno de estos editores en línea. Si el editor en línea que está utilizando no tiene paneles JavaScript / CSS separados, no dude en ponerlos en línea <script> / <style> elementos dentro de la página HTML.
---
+++ 01Tutorials/01Complete beginners/03Bloques de creación de JavaScript/07Galería de imagenes/js/main.js
@@ -0,0 +1,18 @@
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+/* Declarar la matriz de nombres de archivos de imagen */
+
+/* Declarar el texto alternativo para cada archivo de imagen */
+
+/* Recorrer las imágenes en bucle */
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", xxx);
+newImage.setAttribute("alt", xxx);
+thumbBar.appendChild(newImage);
+
+/* Cableado del botón de oscurecimiento/iluminación */
