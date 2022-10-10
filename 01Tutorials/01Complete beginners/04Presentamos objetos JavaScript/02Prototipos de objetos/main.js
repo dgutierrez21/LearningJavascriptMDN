542c6f54d3f15c16e2d6c71071c98dc40f6d5352
Dixon G <dxxn0101@gmail.com>
2022-10-10 16:23:42 -0600

    JS | Prototipos de objetos

---
+++ 01Tutorials/01Complete beginners/04Presentamos objetos JavaScript/02Prototipos de objetos/main.js
@@ -0,0 +1,3 @@
+// Prototipos de objetos #ff0
+
+// Los prototipos son el mecanismo por el cual los objetos JavaScript heredan características entre sí. En este artículo, explicamos qué es un prototipo, cómo funcionan las cadenas de prototipos y cómo se puede establecer un prototipo para un objeto.
