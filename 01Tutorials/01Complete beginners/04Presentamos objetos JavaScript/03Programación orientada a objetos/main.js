9dc6898d785af93b7d2cd36f02b3e09ce31b6432
Dixon G <dxxn0101@gmail.com>
2022-10-10 20:00:08 -0600

    JS | Programación orientada a objetos

---
+++ 01Tutorials/01Complete beginners/04Presentamos objetos JavaScript/03Programación orientada a objetos/main.js
@@ -0,0 +1,9 @@
+// Programación orientada a objetos #ff0
+
+// La programación orientada a objetos (POO) es un paradigma de programación fundamental para muchos lenguajes de programación, incluidos Java y C ++. En este artículo, proporcionaremos una descripción general de los conceptos básicos de POO. Describiremos tres conceptos principales: clases e instancias, herencia y encapsulación. Por ahora, describiremos estos conceptos sin referencia a JavaScript en particular, por lo que todos los ejemplos se dan en pseudocódigo..
+
+// Nota: Para ser precisos, las características descritas aquí son de un estilo particular de POO llamado POO basado en clases o "clásico". Cuando las personas hablan de POO, este es generalmente el tipo al que se refieren.
+
+// Después de eso, en JavaScript, veremos cómo los constructores y la cadena de prototipos se relacionan con estos conceptos de POO y en qué se diferencian. En el próximo artículo, veremos algunas características adicionales de JavaScript que facilitan la implementación de programas orientados a objetos.
+
+// La programación orientada a objetos consiste en modelar un sistema como una colección de objetos, donde cada objeto representa algún aspecto particular del sistema. Los objetos contienen funciones (o métodos) y datos. Un objeto proporciona una interfaz pública a otro código que desea usarlo pero mantiene su propio estado interno privado; otras partes del sistema no tienen que preocuparse por lo que está sucediendo dentro del objeto.
