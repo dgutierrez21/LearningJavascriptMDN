767317118c699c84d22e294f63e9b69411cc2eac
Dixon G <dxxn0101@gmail.com>
2022-10-13 12:18:21 -0600

    JS | No, realmente, ¿qué es JSON?

--- 01Tutorials/01Complete beginners/04Presentamos objetos JavaScript/05Trabajar con datos JSON/main.js
+++ 01Tutorials/01Complete beginners/04Presentamos objetos JavaScript/05Trabajar con datos JSON/main.js
@@ -1,3 +1,12 @@
 // Trabajar con JSON #ff0
 
 // JavaScript Object Notation (JSON) es un formato estándar basado en texto para representar datos estructurados basados en la sintaxis de objetos JavaScript. Se utiliza comúnmente para transmitir datos en aplicaciones web (por ejemplo, enviar algunos datos desde el servidor al cliente, para que puedan mostrarse en una página web, o viceversa). Lo encontrará con bastante frecuencia, por lo que en este artículo, le damos todo lo que necesita para trabajar con JSON usando JavaScript, incluido el análisis de JSON para que pueda acceder a los datos dentro de él y la creación de JSON.
+
+// No, realmente, ¿qué es JSON? #008000
+// JSON es un formato de datos basado en texto que sigue la sintaxis de objetos JavaScript, que fue popularizado por Douglas Crockford. A pesar de que se parece mucho a la sintaxis literal del objeto JavaScript, se puede usar independientemente de JavaScript, y muchos entornos de programación cuentan con la capacidad de leer (analizar) y generar JSON.
+
+// JSON existe como una cadena, útil cuando desea transmitir datos a través de una red. Debe convertirse en un objeto JavaScript nativo cuando desee acceder a los datos. Esto no es un gran problema: JavaScript proporciona un objeto JSON global que tiene métodos disponibles para convertir entre los dos.
+
+// Nota: La conversión de una cadena en un objeto nativo se denomina deserialización, mientras que la conversión de un objeto nativo en una cadena para que pueda transmitirse a través de la red se denomina serialización.
+
+// Una cadena JSON se puede almacenar en su propio archivo, que es básicamente solo un archivo de texto con una extensión de .json y un tipo MIME de application/json.
