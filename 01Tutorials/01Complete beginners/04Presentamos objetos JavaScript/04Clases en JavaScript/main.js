29d83bbaa588e0ffa76b88e9cf37a905f4a244f1
Dixon G <dxxn0101@gmail.com>
2022-10-13 07:19:30 -0600

    JS | Clases en JavaScript

---
+++ 01Tutorials/01Complete beginners/04Presentamos objetos JavaScript/04Clases en JavaScript/main.js
@@ -0,0 +1,7 @@
+// // Clases en JavaScript #ff0
+
+// En el último artículo, presentamos algunos conceptos básicos de la programación orientada a objetos (POO) y discutimos un ejemplo en el que utilizamos los principios de POO para modelar a profesores y estudiantes en una escuela.
+
+// También hablamos sobre cómo es posible usar prototipos y constructores para implementar un modelo como este, y que JavaScript también proporciona características que se ajustan más a los conceptos clásicos de OOP.
+
+// En este artículo, repasaremos estas características. Vale la pena tener en cuenta que las características descritas aquí no son una forma nueva de combinar objetos: bajo el capó, todavía usan prototipos. Son solo una forma de facilitar la configuración de una cadena prototipo.
