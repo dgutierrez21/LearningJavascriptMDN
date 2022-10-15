e4070e22b9f128103301f32f2a7bfc4a370835c7
Dixon G <dxxn0101@gmail.com>
2022-10-15 14:21:42 -0600

    JS | Vamos a rebotar algunas bolas

---
+++ 01Tutorials/01Complete beginners/04Presentamos objetos JavaScript/06Practica de construccion de objetos/main.js
@@ -0,0 +1,8 @@
+// Práctica de construcción de objetos #ff0
+
+// Vamos a rebotar algunas bolas #008000
+// En este artículo escribiremos una demo clásica de "bolas rebotando", para mostrarle lo útiles que pueden ser los objetos en JavaScript. Nuestras pequeñas bolas rebotarán en la pantalla y cambiarán de color cuando se toquen entre sí. El ejemplo terminado se verá un poco como esto:
+
+// ver en la carpeta img...
+
+// Este ejemplo hará uso de la API de Canvas para dibujar las bolas en la pantalla y la API requestAnimationFrame para animar toda la pantalla: no necesita tener ningún conocimiento previo de estas API, y esperamos que para cuando haya terminado este artículo esté interesado en explorarlas más. En el camino, haremos uso de algunos objetos ingeniosos y le mostraremos un par de técnicas agradables como rebotar bolas en las paredes y verificar si se han golpeado entre sí (también conocida como detección de colisiones).).
