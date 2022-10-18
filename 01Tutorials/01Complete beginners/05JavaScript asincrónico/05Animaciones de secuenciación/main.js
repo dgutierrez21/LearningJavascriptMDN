f9af93fcb9658622cfb33f4199ab0a58a5f72653
Dixon G <dxxn0101@gmail.com>
2022-10-18 15:23:13 -0600

    JS | Pasos para completar

--- 01Tutorials/01Complete beginners/05JavaScript asincrónico/05Animaciones de secuenciación/main.js
+++ 01Tutorials/01Complete beginners/05JavaScript asincrónico/05Animaciones de secuenciación/main.js
@@ -34,3 +34,27 @@ const alice3 = document.querySelector("#alice3");
 // La animación ya está definida en "main.js": simplemente gira la imagen y la encoge hasta que desaparece.
 
 // Para que te hagas una idea de cómo queremos que funcione la página, echa un vistazo al ejemplo terminado. Ten en cuenta que las animaciones sólo se ejecutan una vez: para verlas de nuevo, recarga la página.
+
+// Pasos para completar #008000
+// Animar la primera imagen
+// Estamos utilizando la API de Animaciones Web para animar las imágenes, específicamente el método element.animate().
+
+// Actualiza "main.js" para añadir una llamada a alice1.animate(), así:
+
+// alice1.animate(aliceTumbling, aliceTiming);
+
+// Vuelva a cargar la página y debería ver que la primera imagen gira y se encoge.
+
+// Animar todas las imágenes #00aae4
+// A continuación, queremos animar a alice2 cuando alice1 haya terminado, y a alice3 cuando alice2 haya terminado.
+
+// El método animate() devuelve un objeto Animation. Este objeto tiene una propiedad finished, que es una promesa que se cumple cuando la animación ha terminado de reproducirse. Así que podemos usar esta promesa para saber cuándo empezar la siguiente animación.
+
+// impleméntala como una cadena de promesas. Ten en cuenta que hay diferentes maneras de escribir esto, debido a las diferentes formas que puedes usar para una función de flecha. Prueba algunas formas diferentes. ¿Cuál es la más concisa? ¿Cuál te parece más legible?
+
+alice1
+  .animate(aliceTumbling, aliceTiming)
+  .finished.then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+  .then(() => alice3.animate(aliceTumbling, aliceTiming));
+
+// Recuerda que element.animate() no devuelve una Promise: devuelve un objeto Animation con una propiedad finished que es una Promise.
