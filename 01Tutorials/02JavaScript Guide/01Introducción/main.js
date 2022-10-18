d14b0afe3dda485cebfe7ade97eb41166bb790b2
Dixon G <dxxn0101@gmail.com>
2022-10-18 15:37:57 -0600

    JS | Dónde encontrar información sobre JavaScript

--- 01Tutorials/02JavaScript Guide/01Introducción/main.js
+++ 01Tutorials/02JavaScript Guide/01Introducción/main.js
@@ -1,3 +1,10 @@
 // Introducción #ff0
 
 // Este capítulo presenta JavaScript y analiza algunos de sus conceptos fundamentales.
+
+// Dónde encontrar información sobre JavaScript #008000
+// La documentación de JavaScript en MDN incluye lo siguiente:
+
+// Learn Web Development proporciona información para principiantes e introduce conceptos básicos de programación e Internet.
+// La Guía de JavaScript (esta guía) proporciona información general sobre el lenguaje JavaScript y sus objetos.
+// JavaScript Reference proporciona material de referencia detallado para JavaScript.
