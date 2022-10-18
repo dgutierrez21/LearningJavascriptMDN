2bf3a3c843dbccf15e30bfcd86836a454b4179c9
Dixon G <dxxn0101@gmail.com>
2022-10-18 15:41:47 -0600

    JS | ¿Qué es JavaScript?

--- 01Tutorials/02JavaScript Guide/01Introducción/main.js
+++ 01Tutorials/02JavaScript Guide/01Introducción/main.js
@@ -8,3 +8,14 @@
 // Learn Web Development proporciona información para principiantes e introduce conceptos básicos de programación e Internet.
 // La Guía de JavaScript (esta guía) proporciona información general sobre el lenguaje JavaScript y sus objetos.
 // JavaScript Reference proporciona material de referencia detallado para JavaScript.
+
+// ¿Qué es JavaScript? #008000
+// JavaScript es un lenguaje de programación multiplataforma y orientado a objetos que se utiliza para hacer que las páginas web sean interactivas (por ejemplo, con animaciones complejas, botones clicables, menús emergentes, etc.). También existen versiones más avanzadas de JavaScript del lado del servidor, como Node.js, que permiten añadir a un sitio web más funcionalidades que la descarga de archivos (como la colaboración en tiempo real entre varios ordenadores). Dentro de un entorno anfitrión (por ejemplo, un navegador web), JavaScript puede conectarse a los objetos de su entorno para proporcionar un control programático sobre ellos.
+
+// JavaScript contiene una biblioteca estándar de objetos, como Array, Date y Math, y un conjunto básico de elementos del lenguaje, como operadores, estructuras de control y sentencias. El núcleo de JavaScript puede ampliarse para diversos fines complementándolo con objetos adicionales; por ejemplo:
+
+// El JavaScript del lado del cliente amplía el núcleo del lenguaje proporcionando objetos para controlar un navegador y su Modelo de Objetos del Documento (DOM). Por ejemplo, las extensiones del lado del cliente permiten que una aplicación coloque elementos en un formulario HTML y responda a los eventos del usuario, como los clics del ratón, la introducción de datos en el formulario y la navegación por la página.
+
+// El JavaScript del lado del servidor amplía el lenguaje principal proporcionando objetos relevantes para la ejecución de JavaScript en un servidor. Por ejemplo, las extensiones del lado del servidor permiten que una aplicación se comunique con una base de datos, proporcione continuidad de la información de una invocación a otra de la aplicación o realice manipulaciones de archivos en un servidor.
+
+// Esto significa que en el navegador, JavaScript puede cambiar el aspecto de la página web (DOM). Y, del mismo modo, el JavaScript de Node.js en el servidor puede responder a peticiones personalizadas enviadas por el código ejecutado en el navegador.
