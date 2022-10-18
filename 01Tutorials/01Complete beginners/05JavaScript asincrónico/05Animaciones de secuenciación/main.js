60b9ff47df2349a828f4f1d8135a8e051f95209b
Dixon G <dxxn0101@gmail.com>
2022-10-18 13:31:28 -0600

    JS | Secuencia de animaciones

---
+++ 01Tutorials/01Complete beginners/05JavaScript asincrónico/05Animaciones de secuenciación/main.js
@@ -0,0 +1,3 @@
+// Secuencia de animaciones #ff0
+
+// En esta evaluación actualizarás una página para reproducir una serie de animaciones en una secuencia. Para ello, utilizarás algunas de las técnicas que aprendimos en el artículo Cómo usar las promesas.
