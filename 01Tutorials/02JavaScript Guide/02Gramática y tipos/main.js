ec679945566a4438ea30d75c5adf68e08f8756e8
Dixon G <dxxn0101@gmail.com>
2022-10-18 19:40:22 -0600

    JS | Conceptos básicos

--- 01Tutorials/02JavaScript Guide/02Gramática y tipos/main.js
+++ 01Tutorials/02JavaScript Guide/02Gramática y tipos/main.js
@@ -1,3 +1,28 @@
 // Gramática y tipos #ff0
 
 // Este capítulo analiza la gramática básica de JavaScript, las declaraciones de variables, los tipos de datos y los literales.
+
+// Conceptos básicos #008000
+// JavaScript toma prestada la mayor parte de su sintaxis de Java, C y C++, pero también ha recibido la influencia de Awk, Perl y Python.
+
+// JavaScript distingue entre mayúsculas y minúsculas y utiliza el conjunto de caracteres Unicode. Por ejemplo, la palabra Früh (que significa "temprano" en alemán) puede utilizarse como nombre de variable.
+
+const Früh = "foobar";
+
+console.log(Früh);
+
+// Pero, la variable früh no es la misma que Früh porque JavaScript distingue entre mayúsculas y minúsculas.
+
+const früh = "foobar2345";
+
+console.log(früh);
+
+// En JavaScript, las instrucciones se llaman sentencias y se separan con punto y coma (;).
+
+// El punto y coma no es necesario después de una sentencia si se escribe en su propia línea. Pero si se desea más de una sentencia en una línea, entonces deben estar separadas por punto y coma.
+
+// Nota: ECMAScript también tiene reglas para la inserción automática de punto y coma (ASI) para terminar las sentencias. (Para más información, consulte la referencia detallada sobre la gramática léxica de JavaScript).
+
+// Sin embargo, se considera una buena práctica escribir siempre un punto y coma después de una sentencia, incluso cuando no es estrictamente necesario. Esta práctica reduce las posibilidades de que se introduzcan errores en el código.
+
+// El texto fuente del script de JavaScript se escanea de izquierda a derecha, y se convierte en una secuencia de elementos de entrada que son tokens, caracteres de control, terminadores de línea, comentarios o espacios en blanco. (Los espacios, los tabuladores y los caracteres de nueva línea se consideran espacios en blanco).
