a0c70e4af0ee770f6e457c64bf043d654fb81846
Dixon G <dxxn0101@gmail.com>
2022-10-21 15:49:35 -0600

    JS | Flujo de control y manejo de errores

---
+++ 01Tutorials/02JavaScript Guide/03Flujo de control y manejo de errores/main.js
@@ -0,0 +1,7 @@
+// Flujo de control y manejo de errores #ff0
+
+// JavaScript admite un conjunto compacto de sentencias, específicamente sentencias de flujo de control, que puedes utilizar para incorporar una gran cantidad de interactividad en tu aplicación. Este capítulo ofrece una visión general de estas sentencias.
+
+// La referencia de JavaScript contiene detalles exhaustivos sobre las sentencias de este capítulo. El carácter punto y coma (;) se utiliza para separar las sentencias en el código JavaScript.
+
+// Cualquier expresión de JavaScript es también una sentencia. Consulte Expresiones y operadores para obtener información completa sobre las expresiones.
