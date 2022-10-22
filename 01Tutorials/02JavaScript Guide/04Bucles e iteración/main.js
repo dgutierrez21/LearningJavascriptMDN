5fecfa23a65e48f6732b148b019aab0ccae908ca
Dixon G <dxxn0101@gmail.com>
2022-10-22 17:28:32 -0600

    JS | Declaración for

--- 01Tutorials/02JavaScript Guide/04Bucles e iteración/main.js
+++ 01Tutorials/02JavaScript Guide/04Bucles e iteración/main.js
@@ -23,3 +23,45 @@ for (let paso = 0; paso < 5; paso++) {
 // sentencia continue
 // sentencia for...in
 // sentencia for...of
+
+// Declaración for #008000
+// Un bucle for se repite hasta que una condición especificada se evalúa como falsa. El bucle for de JavaScript es similar al bucle for de Java y C.
+
+// Una sentencia for tiene el siguiente aspecto
+
+// for ([expresióninicial]; [expresióncondición]; [expresiónincremento])
+//   declaración
+
+// Cuando un bucle for se ejecuta, ocurre lo siguiente:
+
+// 1. Se ejecuta la expresión inicializadora initialExpression, si la hay. Esta expresión suele inicializar uno o varios contadores del bucle, pero la sintaxis permite una expresión de cualquier grado de complejidad. Esta expresión también puede declarar variables.
+
+// 2. Se evalúa la expresión conditionExpression. Si el valor de conditionExpression es verdadero, las declaraciones del bucle se ejecutan. En caso contrario, el bucle for termina. (Si la expresión conditionExpression se omite por completo, se asume que la condición es verdadera).
+
+// 3. La sentencia se ejecuta. Para ejecutar varias sentencias, utilice una sentencia de bloque ({ }) para agruparlas.
+// 4. Si está presente, se ejecuta la expresión de actualización incrementExpression.
+// 5. El control vuelve al paso 2.
+
+// EJEMPLO
+// En el siguiente ejemplo, la función contiene una sentencia for que cuenta el número de opciones seleccionadas en una lista de desplazamiento (un elemento <select> que permite múltiples selecciones)
+
+// ver html...
+
+// JavaScript #00aae4
+// Aquí, la sentencia for declara la variable i y la inicializa a 0. Comprueba que i es menor que el número de opciones en el elemento <select>, realiza la sentencia if siguiente, e incrementa i en 1 después de cada pasada por el bucle.
+
+function cuantos(seleccionarObjeto) {
+  let numeroSeleccionado = 0;
+  for (let i = 0; i < seleccionarObjeto.options.length; i++) {
+    if (seleccionarObjeto.options[i].selected) {
+      numeroSeleccionado++;
+    }
+  }
+
+  return numeroSeleccionado;
+}
+
+const btn = addEventListener("click", () => {
+  const tiposMusica = document.querySelector("#tiposMusica");
+  console.log(`Has seleccionado ${cuantos(tiposMusica)} opción(es).`);
+});
