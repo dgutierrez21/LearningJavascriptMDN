265f7b18a77cf94a6622b1d3ab31626b19c18f06
Dixon G <dxxn0101@gmail.com>
2022-10-23 21:15:54 -0600

    JS | Funciones

---
+++ 01Tutorials/02JavaScript Guide/05Funciones/main.js
@@ -0,0 +1,5 @@
+// Funciones #008000
+
+// Las funciones son uno de los bloques de construcción fundamentales en JavaScript. Una función en JavaScript es similar a un procedimiento -un conjunto de sentencias que realizan una tarea o calculan un valor-, pero para que un procedimiento pueda calificarse como función, debe tomar alguna entrada y devolver una salida en la que haya alguna relación obvia entre la entrada y la salida. Para utilizar una función, debes definirla en algún lugar del ámbito desde el que deseas llamarla.
+
+// Consulte también el exhaustivo capítulo de referencia sobre las funciones de JavaScript para conocer los detalles.
