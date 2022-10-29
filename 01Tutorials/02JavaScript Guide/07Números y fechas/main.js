85e999ecf0592c839d05ef599b60df1463e084b1
Dixon G <dxxn0101@gmail.com>
2022-10-29 11:50:00 -0600

    JS | Números y fechas

---
+++ 01Tutorials/02JavaScript Guide/07Números y fechas/main.js
@@ -0,0 +1,3 @@
+// Números y fechas #ff0
+
+// Este capítulo presenta los conceptos, objetos y funciones utilizados para trabajar y realizar cálculos con números y fechas en JavaScript. Esto incluye el uso de números escritos en varias bases, incluyendo la decimal, la binaria y la hexadecimal, así como el uso del objeto global Math para realizar una amplia variedad de operaciones matemáticas con números.
