d95173be4bf886a0d7083d8f8caff9a1cb904bed
Dixon G <dxxn0101@gmail.com>
2022-10-30 16:21:02 -0600

    JS | Colecciones indexadas

---
+++ 01Tutorials/02JavaScript Guide/10Colecciones indexadas/main.js
@@ -0,0 +1,7 @@
+// Colecciones indexadas #ff0
+
+// Un array es una lista ordenada de valores a la que se hace referencia con un nombre y un índice.
+
+// Por ejemplo, considere un array llamado emp, que contiene los nombres de los empleados indexados por su número de empleado. Así, emp[0] sería el empleado número cero, emp[1] el empleado número uno, y así sucesivamente.
+
+// JavaScript no tiene un tipo de datos de matriz explícito. Sin embargo, puede utilizar el objeto Array predefinido y sus métodos para trabajar con arrays en sus aplicaciones. El objeto Array tiene métodos para manipular arrays de varias maneras, como unirlos, invertirlos y ordenarlos. Tiene una propiedad para determinar la longitud de la matriz y otras propiedades para su uso con expresiones regulares.
