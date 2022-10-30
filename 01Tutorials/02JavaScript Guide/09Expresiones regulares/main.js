ccbba9b12b17aef37a54eab1dfa48a529ad61d31
Dixon G <dxxn0101@gmail.com>
2022-10-30 08:04:12 -0600

    JS | Expresiones regulares

---
+++ 01Tutorials/02JavaScript Guide/09Expresiones regulares/main.js
@@ -0,0 +1,3 @@
+// Expresiones regulares #ff0
+
+// Las expresiones regulares son patrones que se utilizan para hacer coincidir combinaciones de caracteres en las cadenas. En JavaScript, las expresiones regulares son también objetos. Estos patrones se utilizan con los métodos exec() y test() de RegExp, y con los métodos match(), matchAll(), replace(), replaceAll(), search() y split() de String. Este capítulo describe las expresiones regulares de JavaScript.
