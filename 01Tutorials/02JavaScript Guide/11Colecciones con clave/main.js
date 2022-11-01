0b7050d0609e5e49095286ac6d3cbd1902316b9c
Dixon G <dxxn0101@gmail.com>
2022-11-01 17:20:42 -0600

    JS | Colecciones con llave

---
+++ 01Tutorials/02JavaScript Guide/11Colecciones con clave/main.js
@@ -0,0 +1,3 @@
+// Colecciones con llave #008000
+
+// Este capítulo presenta las colecciones de datos indexadas por una clave; los objetos Map y Set contienen elementos que son iterables en el orden de inserción.
