fc88e1f2453c48806a252f683e4171bfd53883b1
Dixon G <dxxn0101@gmail.com>
2022-11-02 07:11:21 -0600

    JS | Trabajar con objetos

---
+++ 01Tutorials/02JavaScript Guide/12Trabajar con objetos/main.js
@@ -0,0 +1,3 @@
+// Trabajar con objetos #ff0
+
+// JavaScript está diseñado sobre un paradigma simple basado en objetos. Un objeto es una colección de propiedades, y una propiedad es una asociación entre un nombre (o clave) y un valor. El valor de una propiedad puede ser una función, en cuyo caso la propiedad se conoce como método. Además de los objetos predefinidos en el navegador, puedes definir tus propios objetos. Este capítulo describe cómo utilizar objetos, propiedades, funciones y métodos, y cómo crear tus propios objetos.
