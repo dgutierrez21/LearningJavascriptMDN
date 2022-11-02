0a2fa08b5308ec7eda589e7c1259ffbd30dfdbe0
Dixon G <dxxn0101@gmail.com>
2022-11-02 07:15:40 -0600

    JS | Visión general de los objetos

--- 01Tutorials/02JavaScript Guide/12Trabajar con objetos/main.js
+++ 01Tutorials/02JavaScript Guide/12Trabajar con objetos/main.js
@@ -1,3 +1,8 @@
 // Trabajar con objetos #ff0
 
 // JavaScript está diseñado sobre un paradigma simple basado en objetos. Un objeto es una colección de propiedades, y una propiedad es una asociación entre un nombre (o clave) y un valor. El valor de una propiedad puede ser una función, en cuyo caso la propiedad se conoce como método. Además de los objetos predefinidos en el navegador, puedes definir tus propios objetos. Este capítulo describe cómo utilizar objetos, propiedades, funciones y métodos, y cómo crear tus propios objetos.
+
+// Visión general de los objetos #008000
+// Los objetos en JavaScript, al igual que en muchos otros lenguajes de programación, pueden compararse con los objetos de la vida real. El concepto de objetos en JavaScript puede entenderse con objetos tangibles de la vida real.
+
+// En JavaScript, un objeto es una entidad independiente, con propiedades y tipo. Compárelo con una taza, por ejemplo. Una taza es un objeto, con propiedades. Una taza tiene un color, un diseño, un peso, un material del que está hecha, etc. Del mismo modo, los objetos de JavaScript pueden tener propiedades, que definen sus características.
