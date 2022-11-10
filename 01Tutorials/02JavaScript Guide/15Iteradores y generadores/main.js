566b736404ba8f1812decba0371a60d4269c04a5
Dixon G <dxxn0101@gmail.com>
2022-11-10 11:42:48 -0600

    JS | Iteradores y generadores

---
+++ 01Tutorials/02JavaScript Guide/15Iteradores y generadores/main.js
@@ -0,0 +1,10 @@
+// Iteradores y generadores #008000
+
+// Los iteradores y generadores introducen el concepto de iteración directamente en el núcleo del lenguaje y proporcionan un mecanismo para personalizar el comportamiento de los bucles for...of.
+
+// Para más detalles, véase también
+
+// Protocolos de iteración
+// for...of
+// función* y Generator
+// yield y yield*
