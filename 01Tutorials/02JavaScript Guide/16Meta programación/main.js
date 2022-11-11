25d1548f3c6b35f18dd8c3d0d39958f1a50b62cb
Dixon G <dxxn0101@gmail.com>
2022-11-11 10:13:22 -0600

    JS | Meta programación

---
+++ 01Tutorials/02JavaScript Guide/16Meta programación/main.js
@@ -0,0 +1,3 @@
+// Meta programación #ff0
+
+// Los objetos Proxy y Reflect permiten interceptar y definir un comportamiento personalizado para las operaciones fundamentales del lenguaje (por ejemplo, búsqueda de propiedades, asignación, enumeración, invocación de funciones, etc.). Con la ayuda de estos dos objetos se puede programar en el nivel meta de JavaScript.
