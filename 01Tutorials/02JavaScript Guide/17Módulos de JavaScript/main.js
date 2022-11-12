4d3644380c050ea830d6027687d26aece2ea0168
Dixon G <dxxn0101@gmail.com>
2022-11-11 20:31:51 -0600

    JS | Ejemplo de estructura básica

--- 01Tutorials/02JavaScript Guide/17Módulos de JavaScript/main.js
+++ 01Tutorials/02JavaScript Guide/17Módulos de JavaScript/main.js
@@ -19,3 +19,52 @@
 // Son bastante triviales, pero se han mantenido deliberadamente simples para demostrar los módulos con claridad.
 
 // Nota: Si quieres descargar los ejemplos y ejecutarlos localmente, tendrás que ejecutarlos a través de un servidor web local.
+
+// Ejemplo de estructura básica #008000
+
+// En nuestro primer ejemplo (ver módulos básicos) tenemos una estructura de archivos como la siguiente:
+
+// index.html;
+// main.js;
+// modules /
+//      canvas.js;
+//      square.js;
+
+// Nota: Todos los ejemplos de esta guía tienen básicamente la misma estructura; lo anterior debería empezar a ser bastante familiar.
+
+// Los dos módulos del directorio de módulos se describen a continuación:
+
+// canvas.js - contiene funciones relacionadas con la configuración del lienzo:
+
+// create() - crea un lienzo con una anchura y altura especificadas dentro de un <div> envolvente con un ID especificado, que a su vez se anexa dentro de un elemento padre especificado. Devuelve un objeto que contiene el contexto 2D del lienzo y el ID de la envoltura.
+
+// createReportList() - crea una lista desordenada anexada dentro de un elemento envolvente especificado, que puede utilizarse para dar salida a los datos del informe. Devuelve el ID de la lista.
+
+// square.js - contiene:
+
+// name - una constante que contiene la cadena 'square'.
+
+// draw() - dibuja un cuadrado en un lienzo especificado, con un tamaño, posición y color especificados. Devuelve un objeto que contiene el tamaño, la posición y el color del cuadrado.
+
+// reportArea() - escribe el área de un cuadrado en una lista de informes específica, dada su longitud.
+
+// reportPerimeter() - escribe el perímetro de un cuadrado en una lista de informes específica, dada su longitud.
+
+// Aside — .mjs contra .js #00aae4
+
+// A lo largo de este artículo, hemos utilizado las extensiones .js para nuestros archivos de módulos, pero en otros recursos puedes ver que se utiliza la extensión .mjs en su lugar. La documentación de V8 lo recomienda, por ejemplo. Las razones que se dan son:
+
+// Es bueno para la claridad, es decir, deja claro qué archivos son módulos y cuáles son JavaScript normal.
+// Asegura que tus archivos de módulos sean analizados como un módulo por tiempos de ejecución como Node.js, y herramientas de construcción como Babel.
+// Sin embargo, hemos decidido seguir utilizando .js, al menos por el momento. Para que los módulos funcionen correctamente en un navegador, debes asegurarte de que tu servidor los sirve con una cabecera Content-Type que contenga un tipo MIME de JavaScript como text/javascript. Si no lo hace, obtendrá un error de comprobación de tipo MIME estricto del tipo "El servidor respondió con un tipo MIME no JavaScript" y el navegador no ejecutará su JavaScript. La mayoría de los servidores ya establecen el tipo correcto para los archivos .js, pero aún no para los archivos .mjs. Los servidores que ya sirven correctamente los archivos .mjs incluyen GitHub Pages y http-server para Node.js.
+
+// Esto está bien si ya estás usando un entorno de este tipo, o si no lo estás pero sabes lo que estás haciendo y tienes acceso (es decir, puedes configurar tu servidor para establecer el Content-Type correcto para los archivos .mjs). Sin embargo, podría causar confusión si no controlas el servidor desde el que estás sirviendo los archivos, o estás publicando archivos para uso público, como es nuestro caso.
+
+// Por motivos de aprendizaje y portabilidad, hemos decidido mantener el .js.
+
+// Si realmente valoras la claridad de usar .mjs para los módulos frente a usar .js para los archivos JavaScript "normales", pero no quieres encontrarte con el problema descrito anteriormente, siempre puedes usar .mjs durante el desarrollo y convertirlos a .js durante el paso de construcción.
+
+// También hay que tener en cuenta que:
+
+// Algunas herramientas pueden no soportar nunca .mjs.
+// El atributo <script type="module"> se utiliza para denotar cuando se está apuntando a un módulo, como verás a continuación.
