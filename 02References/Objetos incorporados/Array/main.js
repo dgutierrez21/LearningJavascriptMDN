0506ea4960f335955a1cd759864b470e8152429f
Dixon G <dxxn0101@gmail.com>
2022-12-27 20:19:23 -0600

    Descripción

--- 02References/Objetos incorporados/Array/main.js
+++ 02References/Objetos incorporados/Array/main.js
@@ -1,2 +1,13 @@
 // Arreglo #ff0
 // El objeto Array, al igual que con las matrices en otros lenguajes de programación, permite almacenar una colección de varios elementos bajo un solo nombre de variable y tiene miembros para realizar operaciones de matriz comunes..
+
+// Descripción #008000
+// En JavaScript, las matrices no son primitivas, sino objetos Array con las siguientes características principales:
+
+// Las matrices de JavaScript son redimensionables y pueden contener una combinación de diferentes tipos de datos. (Cuando esas características no sean deseables, use matrices con tipo en su lugar).
+
+// Las matrices de JavaScript no son matrices asociativas y, por lo tanto, no se puede acceder a los elementos de matriz utilizando cadenas arbitrarias como índices, sino que se debe acceder mediante enteros no negativos (o su respectiva forma de cadena) como índices.
+
+// Las matrices de JavaScript están indexadas a cero: el primer elemento de una matriz está en el índice 0, el segundo está en el índice 1, y así sucesivamente, y el último elemento está en el valor de la propiedad length de la matriz menos 11.
+
+// Las operaciones de copia de matriz de JavaScript crean copias superficiales. (Todas las operaciones de copia integradas estándar con cualquier objeto JavaScript crean copias superficiales, en lugar de copias profundas.).
