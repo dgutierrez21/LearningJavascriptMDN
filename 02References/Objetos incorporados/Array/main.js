472b5d396841480a6bb43dc97fffe71905936f41
Dixon G <dxxn0101@gmail.com>
2022-12-27 20:15:27 -0600

    Arreglo

---
+++ 02References/Objetos incorporados/Array/main.js
@@ -0,0 +1,2 @@
+// Arreglo #ff0
+// El objeto Array, al igual que con las matrices en otros lenguajes de programación, permite almacenar una colección de varios elementos bajo un solo nombre de variable y tiene miembros para realizar operaciones de matriz comunes..
