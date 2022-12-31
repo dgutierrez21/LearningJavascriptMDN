e630bddb5e8efd2dbe18e9059d8193eff16c3893
Dixon G <dxxn0101@gmail.com>
2022-12-30 19:23:08 -0600

    Creación de cadenas

--- 02References/Objetos incorporados/String/main.js
+++ 02References/Objetos incorporados/String/main.js
@@ -4,3 +4,20 @@
 
 // Descripción #008000
 // Las cadenas son útiles para contener datos que se pueden representar en forma de texto. Algunas de las operaciones más utilizadas en las cadenas son comprobar su length, construirlas y concatenarlas utilizando los operadores de cadena + y +=, comprobar la existencia o ubicación de subcadenas con el método indexOf() o extraer subcadenas con el método indexOf()substring()
+
+// Creación de cadenas #00aae4
+// Las cadenas se pueden crear como primitivas, a partir de literales de cadena o como objetos, utilizando el constructor String()
+
+const string1 = "Una cadena primitiva";
+
+const string2 = "También una cadena primitiva";
+
+const string3 = `Otro cadena primitiva`;
+
+// utilizando constructor
+const string4 = String("Un objeto cadena");
+
+// Las primitivas de cadena y los objetos de cadena comparten muchos comportamientos, pero tienen otras diferencias y advertencias importantes. Consulte "Primitivas de cadena y objetos de cadena" a continuación.
+
+// Los literales de cadena se pueden especificar usando comillas simples o dobles, que se tratan de manera idéntica, o usando el carácter de marca de retroceso `. Este último formulario especifica un literal de plantilla: con este formulario puede interpolar expresiones.
+
