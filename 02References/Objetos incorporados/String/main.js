cc5e7dd1844cbe9da56cc729919399c991ec9bf9
Dixon G <dxxn0101@gmail.com>
2022-12-30 19:12:26 -0600

    Descripción

--- 02References/Objetos incorporados/String/main.js
+++ 02References/Objetos incorporados/String/main.js
@@ -1,3 +1,6 @@
 // String #ff0
 
 // El objeto String se utiliza para representar y manipular una secuencia de caracteres.
+
+// Descripción #008000
+// Las cadenas son útiles para contener datos que se pueden representar en forma de texto. Algunas de las operaciones más utilizadas en las cadenas son comprobar su length, construirlas y concatenarlas utilizando los operadores de cadena + y +=, comprobar la existencia o ubicación de subcadenas con el método indexOf() o extraer subcadenas con el método indexOf()substring()
