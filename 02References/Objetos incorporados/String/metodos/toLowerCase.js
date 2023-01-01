7b9408a3e33b109d181262fb0d0616692e1b1c4d
Dixon G <dxxn0101@gmail.com>
2023-01-01 17:19:11 -0600

    String.prototype.toLowerCase()

---
+++ 02References/Objetos incorporados/String/metodos/toLowerCase.js
@@ -0,0 +1,17 @@
+// String.prototype.toLowerCase()
+// El método toLowerCase() devuelve el valor de cadena de llamada convertido a minúsculas.
+
+// Sintaxis
+// toLowerCase()
+
+// Valor devuelto
+// Una nueva cadena que representa la cadena de llamada convertida a minúsculas.
+
+// Descripción
+// El método toLowerCase() devuelve el valor de la cadena convertida a minúsculas. toLowerCase() no afecta al valor de la cadena str en sí.
+
+// Ejemplos;
+
+console.log(string.toLowerCase());
+
+console.log("EL ZORRO MARRÓN".toLowerCase());
