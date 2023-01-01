97d5d10cc176dd9a3ae60a18b651f44370323721
Dixon G <dxxn0101@gmail.com>
2023-01-01 17:55:25 -0600

    String.prototype.toUpperCase()

---
+++ 02References/Objetos incorporados/String/metodos/toUpperCase.js
@@ -0,0 +1,21 @@
+// String.prototype.toUpperCase()
+// El método toUpperCase() devuelve el valor de cadena de llamada convertido a mayúsculas (el valor se convertirá en una cadena si no es una).
+
+// Sintaxis
+// toUpperCase()
+
+// Valor devuelto
+// Una nueva cadena que representa la cadena de llamada convertida a mayúsculas.
+
+// Excepciones
+// TypeError
+// Cuando se llama en null o undefined, por ejemplo, String.prototype.toUpperCase.call(undefined)undefined).
+
+// Descripción
+// El método toUpperCase() devuelve el valor de la cadena convertida a mayúsculas. Este método no afecta al valor de la cadena en sí, ya que las cadenas JavaScript son inmutables.
+
+// Ejemplos
+
+console.log("Mi texto".toUpperCase());
+
+console.log(string.toUpperCase());
